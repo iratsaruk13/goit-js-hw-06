39dd0d592cb4a4ad94afef935faae90ca4207273
iratsaruk13 <115734138+iratsaruk13@users.noreply.github.com>
2023-02-22 20:59:11 +0200

    update task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -8,37 +8,41 @@ const ingredients = [
 ];
 
 
+const makeListOfIngredients = elements => {
+  return elements.map(element => {
+    const ingredientEl = document.createElement('li');
+    ingredientEl.classList.add('item');
+    ingredientEl.textContent = element;
 
+    return ingredientEl;
+
+  })
+}
+
+const elements = makeListOfIngredients(ingredients);
 const ingredientsListEl = document.querySelector('#ingredients');
-
-const createItem = (item) => {
-  const ingredientItemEl = document.createElement('li');
-  ingredientItemEl.textContent = item;
-  ingredientItemEl.classList.add('item');
-  return ingredientItemEl;
-}
-
-const newItems = (array) => {
-  ingredientsListEl.append(...array.map((element) => createItem(element)))
-}
-
-newItems(ingredients);
-
-// console.log(ingredientsListEl);
-
-// ingredients.forEach((ingredient) => {
-// // console.log(ingredient);
-// const liEl = document.createElement('li');
-// liEl.classList.add('item');
-// liEl.textContent = ingredient;
-//   // console.log(liEl);
-// });
-
-// const listEl = document.querySelector('#ingredients');
-// console.log(listEl);
-// // newLiEl.appendChild(liEl);
+ingredientsListEl.append(...elements);
 
 
+
+
+
+
+
+// const ingredientsListEl = document.querySelector('#ingredients');
+
+// const createItem = (item) => {
+//   const ingredientItemEl = document.createElement('li');
+//   ingredientItemEl.textContent = item;
+//   ingredientItemEl.classList.add('item');
+//   return ingredientItemEl;
+// }
+
+// const newItems = (array) => {
+//   ingredientsListEl.append(...array.map((element) => createItem(element)))
+// }
+
+// newItems(ingredients);
 
 
 
